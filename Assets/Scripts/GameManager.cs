be6c1d62a05ea92bec08bd5a6b9923613e2f2b47
Benyamain <yacoobbenyamain@gmail.com>
2023-05-14 02:36:42 -0400

    Did UI tings

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+
+    private void Start() {
+        NewGame();
+    }
+
+    private void NewGame() {
+        score = 0;
+        scoreText.text = score.ToString();
+    }
+
+    public void IncreaseScore() {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
